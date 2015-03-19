3bbfdf180dd307407d5eac83cf544fa7c35cf23c
HowieBenefiel <hben592@gmail.com>
2015-03-19 11:29:27 -0500

    First two exercises. I learned how to make tables and how some tables relate two other tables.

---
+++ ex2.sql
@@ -0,0 +1,21 @@
+CREATE TABLE person (
+  id INTEGER PRIMARY KEY,
+  first_name TEXT,
+  last_name TEXT,
+  age INTEGER
+);
+
+CREATE TABLE pet (
+  id INTEGER PRIMARY KEY,
+  name TEXT,
+  breed TEXT,
+  age INTEGER,
+  dead INTEGER
+);
+
+CREATE TABLE person_pet (
+  person_id INTEGER,
+  ped_id INTEGER
+);
+
+
